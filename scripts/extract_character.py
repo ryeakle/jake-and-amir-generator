4ebf4e1d2b591b966a615dfad4187925cb2e2fea
ryeakle <ryeakle@gmail.com>
2014-06-18 10:35:35 -0700

    Reorganization, made a command line argument version of the text generator.

--- scripts/extract_character.py
+++
@@ -1,9 +0,0 @@
-import parse
-
-script_file = open('scripts.txt', 'r')
-
-lines = '\n'.join(r.fixed[0] for r in parse.findall("MURPH: {}\n",
-  script_file.read()))
-
-character_file = open('murph.txt', 'w')
-character_file.write('%s\n' % lines)
