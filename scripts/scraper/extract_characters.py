3f9018a861ce03d6dd79674750cfbef71b0091d8
David Muller <dmuller@guidebook.com>
2014-10-19 09:37:04 -0700

    Move output to data directory, improve whitespace handling

--- scripts/scraper/extract_characters.py
+++ scripts/scraper/extract_characters.py
@@ -1,7 +1,7 @@
 """
 Creates files for each character.  Puts them in the
 extract_characters_output/ directory. Requires
-'scripts.txt' to be in the current directory.
+'scripts.txt' to be in the `data/` directory.
 
 Defaults to creating a file for all characters, can be configured
 for just one character via the --character option.
@@ -28,6 +28,22 @@ def get_all_characters(script_file_handle):
     return characters_dict.keys()
 
 
+def clean_character_name(character_name):
+    """
+    Remvoe trailing whitespace, '/' and '*'
+    """
+    character_name = character_name.strip()
+
+    # don't allow character_name names with a '/'
+    if "/" in character_name:
+        character_name = character_name.replace('/', '+')
+    # replace '*' with empty string
+    if '*' in character_name:
+        character_name = character_name.replace('*', '')
+
+    return character_name
+
+
 ################
 # "Main":
 ################
@@ -41,12 +57,14 @@ if __name__ == "__main__":
     # parse command line arguments
     args = parser.parse_args()
 
-    output_directory = "extract_characters_output/"
+    data_directory = "../../data/"
+    scripts_path = os.path.join(data_directory, "scripts.txt")
+    output_directory = os.path.join(data_directory, "characters")
 
     try:
-        script_file_handle = open('scripts.txt', 'r')
+        script_file_handle = open(scripts_path, 'r')
     except:
-        print "Need master script file (scripts.txt) in the same directory as this extract_character.py."
+        print "Need master script file (scripts.txt) in the data directory."
         exit()
 
     if args.character:
@@ -56,18 +74,13 @@ if __name__ == "__main__":
         characters = get_all_characters(script_file_handle)
 
     for character in characters:
-        character = character.strip()
-
-        # don't allow character names with a '/'
-        if "/" in character:
-            character = character.replace('/', '+')
-        # replace '*' with empty string
-        if '*' in character:
-            character = character.replace('*', '')
+        character = clean_character_name(character)
 
         script_file_handle.seek(0)
-        lines = '\n'.join(r.fixed[0] for r in parse.findall(character + ": {}\n",
-                                                            script_file_handle.read()))
+
+        # :^ option handles whitespace
+        # e.g. can find strings like "Jake:text" or " Jake: text" etc.
+        lines = '\n'.join(r.fixed[0] for r in parse.findall(character + ": {:^}\n", script_file_handle.read()))
 
         if lines:
             character_file = open(os.path.join(output_directory, character) + '.txt', 'w')
