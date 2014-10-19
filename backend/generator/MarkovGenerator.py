21a300d0d09146de3fafe9857418b785ebe5d811
David Muller <dmuller@guidebook.com>
2014-10-19 16:16:42 -0700

    Move file_to_words out of Markov class

--- backend/generator/MarkovGenerator.py
+++ backend/generator/MarkovGenerator.py
@@ -1,19 +1,21 @@
 import random
 
+def file_to_words(file_path):
+    """
+    Given a path to a file, return the words in that file.
+    """
+    file_handle = open(file_path)
+    data = file_handle.read()
+    words = data.split()
+    return words
+
 class Markov(object):
 
-    def __init__(self, open_file):
+    def __init__(self, words_list):
         self.cache = {}
-        self.open_file = open_file
-        self.words = self.file_to_words()
+        self.words = words_list
         self.word_size = len(self.words)
         self.database()
-
-    def file_to_words(self):
-        self.open_file.seek(0)
-        data = self.open_file.read()
-        words = data.split()
-        return words
 
     def triples(self):
         if len(self.words) < 3:
@@ -30,12 +32,12 @@ class Markov(object):
             else:
                 self.cache[key] = [w3]
 
-    def generate_markov_text(self, size=25):
+    def generate_markov_text(self, length=25):
         seed = random.randint(0, self.word_size-3)
         seed_word, next_word = self.words[seed], self.words[seed+1]
         w1, w2 = seed_word, next_word
         gen_words = []
-        for i in xrange(size):
+        for i in xrange(length):
             gen_words.append(w1)
             w1, w2 = w2, random.choice(self.cache[(w1,w2)])
         gen_words.append(w2)
