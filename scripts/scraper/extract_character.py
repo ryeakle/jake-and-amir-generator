e2fa3534bd15dea0f551044c1777a852d8ac063f
David Muller <dmuller@guidebook.com>
2014-10-18 22:31:18 -0700

    Add command line options to extract_character script

--- scripts/scraper/extract_character.py
+++ scripts/scraper/extract_character.py
@@ -1,9 +1,31 @@
+import argparse
 import parse
 
-script_file = open('scripts.txt', 'r')
 
-lines = '\n'.join(r.fixed[0] for r in parse.findall("MURPH: {}\n",
-  script_file.read()))
+################
+# "Main":
+################
+if __name__ == "__main__":
+    # build command-line parser
+    parser = argparse.ArgumentParser(description='Extract character lines from master script.')
+    parser.add_argument('character_to_extract', help='Character to extract')
 
-character_file = open('murph.txt', 'w')
-character_file.write('%s\n' % lines)
+    # parse command line arguments
+    args = parser.parse_args()
+
+    # user lower case to find character
+    character_to_extract = args.character_to_extract.lower()
+
+    try:
+        script_file = open('scripts.txt', 'r')
+    except:
+        print "Need master script file (scripts.txt) in the same directory as this extract_character.py."
+        exit()
+
+    # try {:^} or {:>} for whitespace
+
+    lines = '\n'.join(r.fixed[0] for r in parse.findall(character_to_extract + ": {}\n",
+                                                        script_file.read()))
+
+    character_file = open(character_to_extract + '.txt', 'w')
+    character_file.write('%s\n' % lines)
