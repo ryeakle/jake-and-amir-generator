0b53016173ea195a54c7eedbde806c4fb0b2bf33
David Muller <davehmuller@gmail.com>
2014-10-20 02:54:13 +0000

    Add path for deployment server

--- jake_and_amir_generator_api/wsgi.py
+++ jake_and_amir_generator_api/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append("/home/ubuntu/jake-and-amir-generator")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jake_and_amir_generator_api.settings")
 
 from django.core.wsgi import get_wsgi_application
